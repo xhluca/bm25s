d105d0304e2bb27a9e550dfefdce4b0dfeaeb35e
xhluca <git@xinghanlu.com>
2024-09-08 00:41:56 -0400

    Add example of using the tokenizer

--- examples/tokenizer_class_example.py
+++ examples/tokenizer_class_example.py
@@ -1,35 +1,89 @@
-import bm25s
+"""
+# Example: Retrieve from pre-built index of SciFact
+
+This script shows how to load an index built with BM25.index and saved with BM25.save, and retrieve
+the top-k results for a set of queries from the SciFact dataset, via the BEIR library.
+"""
+import beir.util
+from beir.datasets.data_loader import GenericDataLoader
 import Stemmer
 
-stemmer = Stemmer.Stemmer('english')
+import bm25s
+from bm25s.utils.beir import BASE_URL
+from bm25s.tokenization import Tokenizer, Tokenized
 
-tokenizer = bm25s.tokenization.Tokenizer(stemmer=stemmer)
 
-# Tokenize a string
-texts = [
-    "This is a test string",
-    "Here is another test string!"
-]
+def main(data_dir="datasets", dataset="scifact"):
+    # Load the queries from BEIR
+    data_path = beir.util.download_and_unzip(BASE_URL.format(dataset), data_dir)
+    loader = GenericDataLoader(data_folder=data_path)
+    corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split='test')
+    corpus_lst = [doc["title"] + " " + doc["text"] for doc in corpus.values()]
+    queries_lst = list(queries.values())
 
-stream = tokenizer.streaming_tokenize(texts)
+    # Initialize the stemmer
+    stemmer = Stemmer.Stemmer("english")
 
-print("1", tokenizer.vocab)
+    # Initialize the Tokenizer with the stemmer
+    tokenizer = Tokenizer(
+        stemmer=stemmer,
+        lower=True, # lowercase the tokens
+        stopwords="english",  # or pass a list of stopwords
+        splitter=r"\w+",  # by default r"(?u)\b\w\w+\b", can also be a function
+    )
 
-print(next(stream))
+    # Tokenize the corpus
+    corpus_tokenized = tokenizer.tokenize(
+        corpus_lst, 
+        update_vocab=True, # update the vocab as we tokenize
+        return_as="ids"
+    )
 
-print("2", tokenizer.vocab)
+    # stream tokenizing the queries, without updating the vocabulary
+    # note: this cannot return as string due to the streaming nature
+    tokenizer_stream = tokenizer.streaming_tokenize(
+        queries_lst, 
+        update_vocab=False
+    )
+    query_ids = []
 
-print(next(stream))
+    for q in tokenizer_stream:
+        # you can do something with the ids here, e.g. retrieve from the index
+        if 1 in q:
+            query_ids.append(q)
 
-print("3", tokenizer.vocab)
+    # you can convert the ids to a Tokenized namedtuple ids and tokens
+    res = tokenizer.to_tokenized_tuple(query_ids)
+    assert res.ids == query_ids
+    assert res.vocab == tokenizer.get_vocab_dict()
+    assert isinstance(res, Tokenized)
+    
+    # You can also get strings
+    query_strs = tokenizer.to_lists_of_strings(query_ids)
+    assert isinstance(query_strs, list)
+    assert isinstance(query_strs[0], list)
+    assert isinstance(query_strs[0][0], str)
 
-print('-'*20)
-tokenizer.reset_vocab()
+    # Let's see how it's all used
+    retriever = bm25s.BM25()
+    retriever.index(corpus_tokenized, leave_progress=False)
 
-for token in tokenizer.streaming_tokenize(texts):
-    print(token)
+    # all of the above can be passed to index a bm25s model
 
-print(tokenizer.tokenize(texts, show_progress=True, leave_progress=True))
+    # e.g. using the ids directly
+    results, scores = retriever.retrieve(query_ids, k=3)
 
-# todo: test with and without stemming, without and without stopwords, with and without lowercasing
-# todo: test inputs to retriever.retrieve() with both list of strings and list of lists of strings and ids and list of of list of ids
+    # or passing the strings
+    results, scores = retriever.retrieve(query_strs, k=3)
+
+    # or passing the Tokenized namedtuple
+    results, scores = retriever.retrieve(res, k=3)
+    
+    # or passing a tuple of ids and vocab dict
+    vocab_dict = tokenizer.get_vocab_dict()
+    results, scores = retriever.retrieve((query_ids, vocab_dict), k=3)
+
+
+if __name__ == "__main__":
+    main()
+
