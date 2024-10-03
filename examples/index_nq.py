f11bbee4dd8cc0c46cf55a3331288365188a5972
xhluca <git@xinghanlu.com>
2024-10-03 00:20:14 -0400

    Refactor index/retrieve examples to remove dependencies and improve readability

--- examples/index_nq.py
+++ examples/index_nq.py
@@ -6,7 +6,7 @@ This shows how to build an index of the natural questions dataset using BM25S.
 To run this example, you need to install the following dependencies:
 
 ```bash
-pip install beir bm25s PyStemmer
+pip install bm25s[core]
 ```
 
 Then, run with:
@@ -15,19 +15,18 @@ Then, run with:
 python examples/index_nq.py
 ```
 """
-import beir.util
-from beir.datasets.data_loader import GenericDataLoader
-import Stemmer  # from PyStemmer
 
 import bm25s
-from bm25s.utils.beir import BASE_URL
+import Stemmer
 
 
 def main(save_dir="datasets", index_dir="bm25s_indices/nq", dataset="nq"):
-    data_path = beir.util.download_and_unzip(BASE_URL.format(dataset), save_dir)
-    corpus, _, __ = GenericDataLoader(data_folder=data_path).load(split="test")
+    print("Downloading the dataset...")
+    bm25s.utils.beir.download_dataset(dataset, save_dir=save_dir)
+    print("Loading the corpus...")
+    corpus = bm25s.utils.beir.load_corpus(dataset, save_dir=save_dir)
     corpus_records = [
-        {'id': k, 'title': v["title"], 'text': v["text"]} for k, v in corpus.items()
+        {"id": k, "title": v["title"], "text": v["text"]} for k, v in corpus.items()
     ]
     corpus_lst = [r["title"] + " " + r["text"] for r in corpus_records]
 
@@ -36,7 +35,13 @@ def main(save_dir="datasets", index_dir="bm25s_indices/nq", dataset="nq"):
 
     retriever = bm25s.BM25(corpus=corpus_records)
     retriever.index(corpus_tokenized)
+    print("Created BM25 index.")
     retriever.save(index_dir)
+    print(f"Saved the index to {index_dir}.")
+    # get memory usage
+    mem_use = bm25s.utils.benchmark.get_max_memory_usage()
+    print(f"Peak memory usage: {mem_use:.2f} GB")
+
 
 if __name__ == "__main__":
     main()
--- examples/retrieve_nq.py
+++ examples/retrieve_nq.py
@@ -7,7 +7,7 @@ the top-k results for a set of queries from the Natural Questions dataset, via B
 To run this example, you need to install the following dependencies:
 
 ```bash
-pip install beir bm25s[full]
+pip install bm25s[core]
 ```
 
 To build an index, please refer to the `examples/index_nq.py` script. You
@@ -23,34 +23,55 @@ Then, run this script with:
 python examples/retrieve_nq.py
 ```
 """
-import beir.util
-from beir.datasets.data_loader import GenericDataLoader
-import Stemmer
 
+import numpy as np
 import bm25s
-from bm25s.utils.beir import BASE_URL
+import Stemmer
+from tqdm import tqdm
+
 
 def main(index_dir="bm25s_indices/nq", data_dir="datasets", dataset="nq", mmap=True):
     if mmap:
         print("Using memory-mapped index (mmap) to reduce memory usage.")
-    
-    # Load the queries from BEIR
-    data_path = beir.util.download_and_unzip(BASE_URL.format(dataset), data_dir)
-    loader = GenericDataLoader(data_folder=data_path)
-    loader._load_queries()
-    queries_lst = list(loader.queries.values())[:1000]
+
+    timer = bm25s.utils.benchmark.Timer("[BM25S]")
+
+    print("Loading the queries...")
+    queries = bm25s.utils.beir.load_queries(dataset, save_dir=data_dir)
+    queries_lst = [q["text"] for q in queries.values()]
 
     # Tokenize the queries
     stemmer = Stemmer.Stemmer("english")
-    queries_tokenized = bm25s.tokenize(queries_lst, stemmer=stemmer)
+    queries_tokenized = bm25s.tokenize(queries_lst, stemmer=stemmer, return_ids=False)
+
+    mem_use = bm25s.utils.benchmark.get_max_memory_usage()
+    print(f"Initial memory usage: {mem_use:.2f} GB")
 
     # Load the BM25 index and retrieve the top-k results
+    print("Loading the BM25 index...")
+    t = timer.start("Loading index")
     retriever = bm25s.BM25.load(index_dir, mmap=mmap, load_corpus=True)
-    results = retriever.retrieve(queries_tokenized, k=20)
-    
+    retriever.backend = "numba"
+    num_docs = retriever.scores['num_docs']
+    timer.stop(t, show=True, n_total=num_docs)
+
+    mem_use = bm25s.utils.benchmark.get_max_memory_usage()
+    print(f"Memory usage after loading the index: {mem_use:.2f} GB")
+
+    print("Retrieving the top-k results...")
+    t = timer.start("Retrieving")
+    results = retriever.retrieve(queries_tokenized, k=10)
+    timer.stop(t, show=True, n_total=len(queries_lst))
+
+    # get memory usage
+    mem_use = bm25s.utils.benchmark.get_max_memory_usage()
+    print(f"Final (peak) memory usage: {mem_use:.2f} GB")
+
+    print("-" * 50)
     first_result = results.documents[0]
-    print(f"First score (# 1 result):{results.scores[0, 0]}")
+    print(f"First score (# 1 result): {results.scores[0, 0]:.4f}")
     print(f"First result (# 1 result):\n{first_result[0]}")
 
+
 if __name__ == "__main__":
-    main()
+    main(mmap=True)
