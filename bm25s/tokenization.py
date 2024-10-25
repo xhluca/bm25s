f77e90d79e9a3b2303d12ec4fbfd35ffa9f5e0d5
Michael Graczyk <michael@mgraczyk.com>
2024-10-24 20:29:26 -0700

    Fix crash tokenizing with empty word_to_id (#72)

--- bm25s/tokenization.py
+++ bm25s/tokenization.py
@@ -273,7 +273,7 @@ class Tokenizer:
 
             if len(doc_ids) == 0 and allow_empty is True:
                 if update_vocab is True and "" not in self.word_to_id:
-                    self.word_to_id[""] = max(self.word_to_id.values()) + 1
+                    self.word_to_id[""] = max(self.word_to_id.values(), default=0) + 1
                 
                 # get the ID for the empty string
                 if "" in self.word_to_id:
